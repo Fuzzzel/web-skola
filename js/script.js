de0ec258b94e05dd013d39aca8c8bdd981dbce06
Daniel Zbihlej <23IB28_ZBIHLEJ@spsjc.cz>
2025-02-26 15:13:58 +0100

    big update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showInfo() {
+    document.getElementById("info-table").style.display = "block";
+}
+
+function closeInfo() {
+    document.getElementById("info-table").style.display = "none";
+}
